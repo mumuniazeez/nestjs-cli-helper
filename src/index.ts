9525bf4d05e803f97d30ff2ae1ec00157eca97bc
mumuniazeez <mumuniazeez99@gmail.com>
2026-07-16 23:03:03 +0100

    fix: correct fallback comment and improve package.json path handling

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,27 @@
 #!/usr/bin/env node
 
+import chalk from "chalk";
 import { Command } from "commander";
 import { readFile, readFileSync } from "fs";
-import path, { dirname } from "path";
+import path, { dirname, join } from "path";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// This is a fal;back version in case somethings fails
+// This is a fallback version in case somethings fails
 let version = "1.0.0";
+
+try {
+  const packageJsonPath = join(__dirname, "..", "package.json");
+  const packageJson = JSON.parse(readFileSync(packageJsonPath, "utf-8"));
+} catch (error) {
+  console.log(
+    chalk.yellow(
+      "Warning: Failed to get version  from package.json, using default 1.0.0 version",
+    ),
+  );
+}
 
 const program = new Command();
 
