9bfb58019b52886fa90ff9943a55dfbe34493cc2
mumuniazeez <mumuniazeez99@gmail.com>
2026-07-16 22:57:00 +0100

    fix: correct description in package.json and update types in tsconfig.json

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
 #!/usr/bin/env node
 
 import { Command } from "commander";
+import { readFile, readFileSync } from "fs";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// This is a fal;back version in case somethings fails
+let version = "1.0.0";
 
 const program = new Command();
 
-program.name("Nestjs Helper")
+program
+  .name("Nestjs Helper")
+  .description("A Nestjs Cli helper that help you install Nestjs modules")
+  .version(version);
 
 program.parse();
